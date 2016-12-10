518827ff6b1dc31c347f9b15cfeb0bc2de2b6bd3
Martin Pinter <mpinter.pinto@gmail.com>
2016-12-10 15:34:57 +0100

    Input

--- ld37/Assets/Scripts/InputScript.cs
+++ ld37/Assets/Scripts/InputScript.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using flyyoufools;
 
 public class InputScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+	public ReactiveProperty<bool> MovementUnlocked { get; set; }
+	public IObservable<Vector2> Movement { get; private set; }
+	private void Awake() {
+		Movement = this.FixedUpdateAsObservable()
+		.Throttle(System.TimeSpan.FromSeconds(1))
+		.Select(_ => {
+			var x = Input.GetAxis("Horizontal");
+			var y = Input.GetAxis("Vertical");
+			return new Vector2(x, y).normalized;
+		});
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Start () {
 		
 	}
 }
