5aaad49fdd06f6e6dbf0bd9cda7367028bc1a347
Martin Pinter <mpinter.pinto@gmail.com>
2016-12-10 15:56:01 +0100

    Map script minor things

--- ld37/Assets/Scripts/MapScript.cs
+++ ld37/Assets/Scripts/MapScript.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class MapScript : MonoBehaviour {
 
+	public int width = 10;
+	public int height = 10;
+
+	public IObservable<Tile[][]> Tiles { get; private set; } 	
+
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Logujem nieco ?");
@@ -12,7 +17,8 @@ public class MapScript : MonoBehaviour {
 		.Where(v => v != Vector2.zero)
 		.Subscribe( todo => {
 			Debug.Log("Hello");
-		});
+		})
+		.AddTo(this);
 	}
 	
 	// Update is called once per frame
