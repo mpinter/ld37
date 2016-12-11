f600ecc328e7b40e259c5c56c0730bc071642d65
Martin Pinter <mpinter.pinto@gmail.com>
2016-12-12 00:14:42 +0100

    Mana bar

---
+++ ld37/Assets/Scripts/UIAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAnimation : MonoBehaviour {
+    Image img;
+    SpriteRenderer sprite;
+ 
+    // Use this for initialization
+    void Start () {
+        img = GetComponent<Image>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+   
+    // Update is called once per frame
+    void Update () {
+        img.sprite = sprite.sprite;
+    }
+}
