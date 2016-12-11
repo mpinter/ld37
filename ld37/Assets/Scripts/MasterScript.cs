49b0555b8cdd386989c407c047c2864147929b87
Martin Pinter <mpinter.pinto@gmail.com>
2016-12-11 15:53:56 +0100

    UI

--- ld37/Assets/Scripts/MasterScript.cs
+++ ld37/Assets/Scripts/MasterScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using flyyoufools;
 using UniRx;
 using System;
+using UnityEngine.UI;
 
 public class MasterScript : MonoBehaviour {
 
 	public int sanity = 10;
+	public int sanityMax = 10;
+	public GameObject sanityBar;
 
 	// Use this for initialization
 	void Start () {
@@ -20,19 +23,39 @@ public class MasterScript : MonoBehaviour {
 			enemyTurn();
 		})
 		.AddTo(this);
+		sanityBar.GetComponentInChildren<Image>().fillMethod=Image.FillMethod.Vertical;
+        sanityBar.GetComponentInChildren<Image>().type=Image.Type.Filled;
+		sanityBar.GetComponentInChildren<Image>().enabled = true;
 	}
 
 	void enemyTurn() {
+		sanity = Math.Min(10, sanity+4);
 		this.gameObject.GetComponent<MapScript>().enemyTurn();
 		//TODO all of the other stuff
 	}
 
+	public void movePlayer() {
+		sanity -= 1;
+	}
+
+	public void movePossesed() {
+		sanity -= 3;
+	}
+
+	public void moveUnpossesed() {
+		sanity -= 2;
+	}
+
+	void Update() {
+		Debug.Log(sanityBar.GetComponentInChildren<Image>().fillAmount);
+		sanityBar.GetComponentInChildren<Image>().fillAmount=(float)sanity/(float)sanityMax;
+		// heh ,this is lame
+		if (sanity < 1) {
+			gameOver();
+		}
+	}
+
 	public void gameOver() {
 		Debug.Log("To be called");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
