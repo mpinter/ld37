b3c605bd303f2fcc7a45ead40ba80b361d4fe4d5
Martin Strapko <wryxsk@gmail.com>
2016-12-11 19:21:02 +0100

    Blasphemous Act!

---
+++ ld37/Assets/Scripts/Possessable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Possessable : MonoBehaviour {
+
+	GameObject pentagram;
+	GameObject svetlo;
+
+	// Use this for initialization
+	void Start () {
+		pentagram = transform.Find("pentagram").gameObject;
+		svetlo = transform.Find("light").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tag == "Enemy" && !pentagram.activeSelf) {
+			pentagram.SetActive(true);
+			svetlo.SetActive(true);
+		} else if (tag != "Enemy" && pentagram.activeSelf) {
+			pentagram.SetActive(false);
+			svetlo.SetActive(false);
+		}
+	}
+}
