b8487567bff443dee47e77b05421b504f695ad8a
Martin Strapko <wryxsk@gmail.com>
2016-12-12 20:28:32 +0100

    UI font update

--- ld37/Assets/Levels/test.cs
+++ ld37/Assets/Levels/test.cs
@@ -67,49 +67,29 @@ namespace flyyoufools {
             },
         // maps 2
             new string[]{
-            "r e e e e e e C",
-            "e e f e e e w e",
-            "e r e r e w e e",
-            "e e c p e e e e",
-            "e e e e e c e e",
-            "e e w e r e r e",
-            "e w e e e f e e",
-            "f e e e e e e r"
-            },
-            new string[]{
             "X e e e e e e c",
             "e e f e e e w e",
-            "e r e r e w e e",
+            "e r e r e W e e",
             "e e c p e e e e",
             "e e e e e c e e",
-            "e e w e r e r e",
+            "e e W e r e r e",
             "e w e e e f e e",
             "f e e e e e e R"
             },
             new string[]{
             "X e e e e e e C",
             "e e f e e e w e",
-            "e r e r e w e e",
+            "e r e r e W e e",
             "e e c p e e e e",
             "e e e e e c e e",
-            "e e w e r e r e",
+            "e e W e r e r e",
             "e w e e e f e e",
             "F e e e e e e R"
             },
         // maps 0
             new string[]{
             "e e e e r e e e",
-            "w r w w w e c e",
-            "e e e e r e e e",
-            "e e e c c e e e",
-            "e e e e c r e e",
-            "e F w e e f e e",
-            "p e f e e f e e",
-            "c e f e e e e f"
-            },
-            new string[]{
-            "e e e e r e e e",
-            "w r w w w e c e",
+            "w r W w W e c e",
             "e e e e r e e e",
             "e e e c c e e e",
             "e e e e c r e e",
@@ -119,7 +99,7 @@ namespace flyyoufools {
             },
             new string[]{
             "e e e e R e e e",
-            "w r w w w e c e",
+            "w r W w W e c e",
             "e e e e r e e e",
             "e e e c c e e e",
             "e e e e c r e e",
@@ -129,7 +109,7 @@ namespace flyyoufools {
             },
             new string[]{
             "e e e e R e e e",
-            "w r w w w e c e",
+            "w r W w W e c e",
             "e e e e r e e e",
             "e e e c c e e e",
             "e e e e C r e e",
@@ -146,7 +126,7 @@ namespace flyyoufools {
             "e e c e e w e e",
             "e e e e e e w e",
             "p e c w e f e e",
-            "e e e w F e e e"
+            "e e e W F e e e"
             },
             new string[]{
             "e e R e f e e e",
@@ -166,7 +146,7 @@ namespace flyyoufools {
             "e e c e e w e e",
             "e e e e e e w e",
             "p e C w e f e e",
-            "e e e w F e e e"
+            "e e e W F e e e"
             },
          
         };
